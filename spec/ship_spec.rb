260bdc136d620bf07878a02b452664693ee11708
Jaren Garman <jarengarman@protonmail.com>
2024-12-21 11:28:57 -0500

    test: Add test for health method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,19 +1,21 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 RSpec.describe Ship do
-  subject { described_class.new("Cruiser", 3) }
+  subject(:ship) { described_class.new('Cruiser', 3) }
 
   describe '#initialize' do
-    it 'exists' do
-      expect(subject).to be_instance_of(Ship)
-    end
+    it { is_expected.to be_instance_of(described_class) }
 
     it 'has a name' do
-      expect(subject.name).to eq("Cruiser")
+      expect(ship.name).to eq('Cruiser')
     end
 
     it 'has a length' do
-      expect(subject.length).to eq(3)
+      expect(ship.length).to eq(3)
+    end
+
+    it 'has health equal to length' do
+      expect(ship.health).to eq(3)
     end
   end
 end
