0d44f2755c207892a6323c8fded463074b3d4ed5
Donte Handy <donte.handy@gmail.com>
2024-12-22 14:15:07 -0400

    feat: Add fired_upon functionality to Cell class and health management to Ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,18 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length # Initially, the health of the ship is equal to its length
+  end
+
+  def hit
+    @health -= 1 if @health > 0 # Reduce health by 1 if health is greater than 0
+  end
+
+  def sunk?
+    @health == 0 # The ship is sunk if its health is 0
   end
 end
+
