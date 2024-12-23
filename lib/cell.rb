600586f2434eae72164d65cf9286ffd555d65aba
Jaren Garman <jarengarman@protonmail.com>
2024-12-22 20:06:02 -0500

    docs: Comment formatting

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,28 +1,34 @@
+# Create a cell with a coordinate. Can add a ship and fire upon.
 class Cell
   attr_reader :coordinate, :ship
 
-  def initialize(coordinate) #(coordinate) is an argument passed to the initialize method - ie "B4"
+  # (coordinate) is an argument passed to the initialize method - ie "B4"
+  def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
-    @fired_upon = false 
+    @fired_upon = false
   end
 
-  def empty? #empty? method checks if a cell is empty
+  # empty? method checks if a cell is empty
+  def empty?
     @ship.nil?
   end
 
-  def place_ship(ship) #place_ship method takes a ship as an argument. it's function is to place a ship in a cell
+  # place_ship method takes a ship as an argument. it's function is to place a ship in a cell
+  def place_ship(ship)
     @ship = ship
   end
 
-  def fired_upon? #fired_upon? method checks if a cell has been fired upon
+  # fired_upon? method checks if a cell has been fired upon
+  def fired_upon?
     @fired_upon
   end
 
-  def fire_upon #fire_upon method marks a cell as fired upon
+  # fire_upon method marks a cell as fired upon
+  def fire_upon
     @fired_upon = true
-    @ship.hit unless empty? #calls the hit method on the ship if the cell is not empty
+    @ship.hit unless empty? # calls the hit method on the ship if the cell is not empty
   end
 end
 
-#fire_upon? & fire_upon are in Cell class because they are methods that are specific to the Cell class. 
+# fire_upon? & fire_upon are in Cell class because they are methods that are specific to the Cell class.
