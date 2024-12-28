dc95735c7cee20688cdbf8d689ad38df26362ed8
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 15:55:49 -0500

    refactor: Comment formatting

--- run_game.rb
+++ run_game.rb
@@ -1,7 +1,6 @@
 require_relative 'lib/game'
 
-game = Game.new #creates a new instance of the Game class
-game.display_main_menu #calls the display_main_menu method on the game instance
-
+game = Game.new # creates a new instance of the Game class
+game.display_main_menu # calls the display_main_menu method on the game instance
 
 # ruby run_game.rb - to play the game
