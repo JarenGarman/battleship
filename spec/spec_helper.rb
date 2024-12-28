ca71d5898ee3e2b72939d785e159299b09d09989
Donte Handy <donte.handy@gmail.com>
2024-12-28 15:52:38 -0400

    refactor: Clean up comments in spec_helper.rb for clarity and consistency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ require_relative '../lib/game'
 RSpec.configure(&:disable_monkey_patching!)
 
 
-# #we'll need two more classess to test the Game class completely
-# #we'll need a Player class and a ComputerPlayer class
-# #the Player class will be responsible for handling the player's moves and the ComputerPlayer class will be responsible for handling the computer's moves
-# #we'll need to create a new instance of the Player class and ComputerPlayer class in the Game
+#we'll need two more classes to test the Game class completely
+#we'll need a Player class and a ComputerPlayer class
+#the Player class will be responsible for handling the player's moves and the ComputerPlayer class will be responsible for handling the computer's moves
