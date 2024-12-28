840ca58407c74e711da1cc190c151f8d4dbd33c2
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 15:52:13 -0500

    refactor: Comment formatting, remove input, add attr_reader for game_over

--- lib/game.rb
+++ lib/game.rb
@@ -1,40 +1,43 @@
+# Play the game!
 class Game
+  attr_reader :game_over
+
   def initialize
     @game_over = false
   end
+
   # The display_main_menu method is the entry point for the game. It displays the main menu and handles user input.
   def display_main_menu
-    puts "Welcome to BATTLESHIP"
+    puts 'Welcome to BATTLESHIP'
     puts "Enter 'p' to play. Enter 'q' to quit."
     handle_main_menu_input
   end
 
-  private #used internally by the Game class.
+  private # used internally by the Game class.
 
   # reads user input and determines the next steps based on the input.
   def handle_main_menu_input
-    input = gets.chomp.downcase
-    case input
+    case gets.chomp.downcase
     when 'p'
       start_game
     when 'q'
-      puts "Quitting game..."
+      puts 'Quitting game...'
       exit
     else
       puts "Invalid input. Please enter 'p' to play or 'q' to quit."
-      display_main_menu
+      handle_main_menu_input
     end
   end
 
   # called when the user chooses to start the game. contains the game logic/calls the end_game method when over.
   def start_game
-    puts "Starting game..."
+    puts 'Starting game...'
     # Add logic to start the game
     end_game
   end
 
   def end_game
-    puts "Game over. Returning to main menu..."
+    puts 'Game over. Returning to main menu...'
     display_main_menu
   end
 end
