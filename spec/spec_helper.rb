87164a27e83aa72319e5108bf0d4bd21615af7a2
Donte Handy <donte.handy@gmail.com>
2024-12-28 15:38:59 -0400

    chore: Update spec_helper.rb to prepare for Player and ComputerPlayer class integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,13 @@ require_relative '../lib/ship'
 require_relative '../lib/cell'
 require_relative '../lib/board'
 require_relative '../lib/game'
-
+# require_relative '../lib/player'
+# require_relative '../lib/computer_player'
 
 RSpec.configure(&:disable_monkey_patching!)
+
+
+# #we'll need two more classess to test the Game class completely
+# #we'll need a Player class and a ComputerPlayer class
+# #the Player class will be responsible for handling the player's moves and the ComputerPlayer class will be responsible for handling the computer's moves
+# #we'll need to create a new instance of the Player class and ComputerPlayer class in the Game
