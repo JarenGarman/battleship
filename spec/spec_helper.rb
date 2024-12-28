46f13ce130dad0018acd6658c486f743911db81e
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 15:42:59 -0500

    test: Add computer to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,16 @@ SimpleCov.start do
 end
 require 'pry'
 require 'rspec'
+
 require_relative '../lib/ship'
 require_relative '../lib/cell'
 require_relative '../lib/board'
 require_relative '../lib/game'
+require_relative '../lib/computer'
 # require_relative '../lib/player'
-# require_relative '../lib/computer_player'
 
 RSpec.configure(&:disable_monkey_patching!)
 
-
-#need two more classes to test the Game class completely
-#Player class and a ComputerPlayer class
-#Player class for handling the player's moves + ComputerPlayer classfor handling the computer's moves
+# need two more classes to test the Game class completely
+# Player class and a ComputerPlayer class
+# Player class for handling the player's moves + ComputerPlayer class for handling the computer's moves
