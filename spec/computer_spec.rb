2e02f3eb5e5e9cc9a8c38d1f3e4ff866c5a6e91e
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 19:51:05 -0500

    test: Add test for placing ships

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe Computer do
     it 'has a board' do
       expect(computer.board).to be_instance_of Board
     end
+  end
+
+  describe '#place_ships' do
+    let(:cruiser) { Ship.new('Cruiser', 3) }
+    let(:submarine) { Ship.new('Submarine', 2) }
+    let(:ships) { [cruiser, submarine] }
 
     it 'places submarine and cruiser' do
+      computer.place_ships(ships)
+
       expect(computer.board.render(true).count('S')).to eq(5)
     end
   end
