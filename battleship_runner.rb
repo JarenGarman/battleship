7065cf230632a24735ea054d108139773e377128
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 19:38:48 -0500

    refactor: Rename run_game to battleship_runner and remove game.start call

---
+++ battleship_runner.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/game'
+
+Game.new # creates a new instance of the Game class to start the game
+
+# ruby run_game.rb - to play the game
--- run_game.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'lib/game'
-
-game = Game.new # creates a new instance of the Game class
-game.start # calls the start method on the game instance
-
-# ruby run_game.rb - to play the game
