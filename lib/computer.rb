6b87acf13fdced3984e52bd53f6117472fbf8ac4
Jaren Garman <jarengarman@protonmail.com>
2024-12-30 15:05:17 -0500

    refactor: Computer generalization, readability, and extensability

--- lib/computer.rb
+++ lib/computer.rb
@@ -5,8 +5,8 @@ require_relative 'ship'
 class Computer
   attr_reader :board
 
-  def initialize
-    @board = Board.new
+  def initialize(board = Board.new)
+    @board = board
   end
 
   def place_ships(ships)
@@ -21,33 +21,27 @@ class Computer
     coords = []
     i = 0
     until @board.valid_placement?(ship, coords)
-      horizontal_bool = Random.new.rand(2).zero?
-      start_coord = generate_start_coord(ship.length, horizontal_bool)
-      coords = generate_remaining_coords(ship.length, horizontal_bool, start_coord)
+      horizontal, vertical = [0, 1].shuffle # rubocop:disable Performance/CollectionLiteralInLoop
+      start_coord = generate_start_coord(ship.length, horizontal, vertical)
+      coords = generate_remaining_coords(ship.length, horizontal, vertical, start_coord)
       i += 1
       return if i == 100
     end
     coords
   end
 
-  def generate_start_coord(length, horizontal)
+  def generate_start_coord(ship_length, horizontal, vertical) # rubocop:disable Metrics/AbcSize
     @board.cells.values.select do |cell|
-      if horizontal
-        cell.coordinate[1].to_i < 6 - length
-      else
-        cell.coordinate[0].ord < 70 - length
-      end && cell.empty?
+      cell.coordinate[0].ord < (66 + @board.rows.length) - (ship_length * horizontal) &&
+        cell.coordinate[1].to_i < (2 + @board.columns.length) - (ship_length * vertical) &&
+        cell.empty?
     end.sample.coordinate
   end
 
-  def generate_remaining_coords(length, horizontal, start_coord)
+  def generate_remaining_coords(ship_length, horizontal, vertical, start_coord)
     coords = []
-    length.times do |i|
-      coords << if horizontal
-                  "#{start_coord[0]}#{start_coord[1].to_i + i}"
-                else
-                  "#{(start_coord[0].ord + i).chr}#{start_coord[1]}"
-                end
+    ship_length.times do |i|
+      coords << "#{(start_coord[0].ord + (i * vertical)).chr}#{start_coord[1].to_i + (i * horizontal)}"
     end
     coords
   end
