fd671b92a58f35da9e62ed335403549dd71b42a7
Donte Handy <donte.handy@gmail.com>
2025-01-02 14:17:24 -0400

    fix: Reorder place_ship and fired_upon? methods for improved clarity

--- lib/cell.rb
+++ lib/cell.rb
@@ -9,25 +9,25 @@ class Cell
     @fired_upon = false
   end
 
-  # place_ship method takes a ship as an argument. it's function is to place a ship in a cell
-  def place_ship(ship)
-    @ship = ship
-  end
-
   # empty? method checks if a cell is empty
   def empty?
     @ship.nil?
   end
 
-  # fired_upon? method checks if a cell has been fired upon
-  def fired_upon?
-    @fired_upon
+  # place_ship method takes a ship as an argument. it's function is to place a ship in a cell
+  def place_ship(ship)
+    @ship = ship
   end
 
   # fire_upon method marks a cell as fired upon
   def fire_upon
     @fired_upon = true
     @ship.hit if @ship # calls the hit method on the ship if the cell is not empty
+  end
+
+  # fired_upon? method checks if a cell has been fired upon
+  def fired_upon?
+    @fired_upon
   end
 
   def render(show_ship = false)
