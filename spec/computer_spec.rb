2e5161d5158afebea6689462d1f871c6a92451d9
Jaren Garman <jarengarman@protonmail.com>
2025-01-04 17:20:06 -0500

    test: Add tests for add_ships methods

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,21 +3,33 @@ require_relative 'spec_helper'
 RSpec.describe Computer do
   subject(:computer) { described_class.new }
 
+  let(:cruiser) { Ship.new('Cruiser', 3) }
+  let(:submarine) { Ship.new('Submarine', 2) }
+  let(:ships) { [cruiser, submarine] }
+
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
 
     it 'has a board' do
       expect(computer.board).to be_instance_of Board
     end
+
+    it 'has no ships' do
+      expect(computer.ships).to eq([])
+    end
+  end
+
+  describe '#add_ships' do
+    it 'can add ships' do
+      computer.add_ships(ships)
+
+      expect(computer.ships).to eq([cruiser, submarine])
+    end
   end
 
   describe '#place_ships' do
-    let(:cruiser) { Ship.new('Cruiser', 3) }
-    let(:submarine) { Ship.new('Submarine', 2) }
-    let(:ships) { [cruiser, submarine] }
-
     it 'places submarine and cruiser' do
-      computer.place_ships(ships)
+      computer.place_ships
 
       expect(computer.board.render(true).count('S')).to eq(5)
     end
