eca730299a006fa52827d5d93ac58164a07d30e5
Jaren Garman <jarengarman@protonmail.com>
2025-01-04 16:07:26 -0500

    test: Remove tests for user input, add test for initialize

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,39 +1,13 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 RSpec.describe User do
-  let(:user) { described_class.new }
-  let(:board) { instance_double(Board) }
-  let(:cruiser) { Ship.new('Cruiser', 3) }
-  let(:submarine) { Ship.new('Submarine', 2) }
-
-  before do
-    allow(Board).to receive(:new).and_return(board)
-    allow(board).to receive(:render).and_return("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
-    allow(board).to receive(:valid_placement?).and_return(true)
-    allow(board).to receive(:place)
-  end
+  subject(:user) { described_class.new }
 
   describe '#initialize' do
+    it { is_expected.to be_instance_of described_class }
+
     it 'initializes with a new board' do
-      expect(user.board).to eq(board)
-    end
-  end
-
-  describe '#place_ships' do
-    before do
-      allow(user).to receive(:gets).and_return('A1 A2 A3', 'B1 B2')
-    end
-
-    it 'prompts the user to place ships and displays the board' do
-      expect { user.place_ships([cruiser, submarine]) }.to output(/Enter the squares for the Cruiser \(3 spaces\):\n  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\nEnter the squares for the Submarine \(2 spaces\):\n  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n/).to_stdout
-      expect(board).to have_received(:place).with(cruiser, ['A1', 'A2', 'A3'])
-      expect(board).to have_received(:place).with(submarine, ['B1', 'B2'])
-    end
-  end
-
-  describe '#render_board' do
-    it 'displays the current state of the board' do
-      expect { user.render_board }.to output("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n").to_stdout
+      expect(user.board).to be_instance_of Board
     end
   end
 end
