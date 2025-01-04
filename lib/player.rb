516d8a609ca586cd37e6f0175dde51a246dd476e
Jaren Garman <jarengarman@protonmail.com>
2025-01-04 17:09:11 -0500

    refactor: Rename User to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,27 +1,35 @@
 require_relative 'ship'
 require_relative 'board'
 
+# Enemy of the computer
 class Player
   attr_reader :board
 
-  def initialize(board)
-    @board = board
+  def initialize
+    @board = Board.new
   end
 
   def place_ships(ships)
     ships.each do |ship|
-      placed = false
-      until placed
-        puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
-        coordinates = gets.chomp.split
-        if @board.valid_placement?(ship, coordinates)
-          @board.place(ship, coordinates)
-          placed = true
-          puts "DEBUG: #{ship.name} placed at #{coordinates.inspect}" if DEBUG_MODE
-        else
-          puts "Those are invalid coordinates. Please try again."
-        end
-      end
+      puts @board.render(true)
+      puts
+      @board.place(ship, get_valid_coordinates(ship))
     end
+  end
+
+  private
+
+  def get_valid_coordinates(ship) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    puts
+    puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+    coordinates = gets.chomp.upcase.split
+    puts
+    until @board.valid_placement?(ship, coordinates)
+      puts
+      puts "Invalid coordinates. Please enter the squares for the #{ship.name} (#{ship.length} spaces):"
+      coordinates = gets.chomp.upcase.split
+      puts
+    end
+    coordinates
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,13 @@
 require_relative 'spec_helper'
 
-RSpec.describe User do
-  subject(:user) { described_class.new }
+RSpec.describe Player do
+  subject(:player) { described_class.new }
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
 
     it 'initializes with a new board' do
-      expect(user.board).to be_instance_of Board
+      expect(player.board).to be_instance_of Board
     end
   end
 end
