4c650007209ca955c72c6b2b1e7308c67758f4d0
Donte Handy <donte.handy@gmail.com>
2025-01-06 22:13:34 -0400

    feat: Implement target hunting logic and random guessing for computer player

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -17,6 +17,10 @@ RSpec.describe Computer do
     it 'has no ships' do
       expect(computer.ships).to eq([])
     end
+
+    it 'has no targets' do
+      expect(computer.targets).to eq([])
+    end
   end
 
   describe '#add_ships' do
@@ -33,6 +37,38 @@ RSpec.describe Computer do
       computer.place_ships
 
       expect(computer.board.render(true).count('S')).to eq(5)
+    end
+  end
+
+  describe '#hunt_target' do
+    it 'returns a random guess when there are no targets' do
+      guess = computer.hunt_target
+      expect(guess[0]).to be_between(0, computer.board.rows.length - 1)
+      expect(guess[1]).to be_between(0, computer.board.columns.length - 1)
+    end
+
+    it 'returns the last target when targets are available' do
+      computer.instance_variable_set(:@targets, [[1, 1], [2, 2]])
+      expect(computer.hunt_target).to eq([2, 2])
+    end
+
+    it 'adds adjacent cells to targets when a ship is hit' do
+      computer.add_ships([cruiser])
+      computer.place_ships
+      computer.instance_variable_set(:@targets, [[1, 1]])
+      cell = computer.board.cells["B2"]
+      cell.place_ship(cruiser)
+      cell.fire_upon
+      computer.hunt_target
+      expect(computer.targets).to include([2, 1], [1, 2], [0, 1], [1, 0])
+    end
+  end
+
+  describe '#guess_random' do
+    it 'returns a random coordinate within the board' do
+      guess = computer.send(:guess_random)
+      expect(guess[0]).to be_between(0, computer.board.rows.length - 1)
+      expect(guess[1]).to be_between(0, computer.board.columns.length - 1)
     end
   end
 end
